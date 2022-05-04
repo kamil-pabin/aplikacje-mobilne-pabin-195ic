fc1cfb7cccebad92679e84a4777470ea38df40f4
Kamil Pabin <kpabin9@gmail.com>
2022-05-04 15:48:30 +0200

    Lab4 zad1

--- renedrowanie-listy-danych-progres/screens/TabOneScreen.tsx
+++ renedrowanie-listy-danych-progres/screens/TabOneScreen.tsx
@@ -1,66 +1,96 @@
 import { StyleSheet, Button, Image, TextInput, ImageBackground } from 'react-native';
-
+import React from "react";
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabOneScreen(this: any, { navigation }: RootTabScreenProps<'TabOne'>) {
   const style = require('./styles');
-
-
-  const liczby = [];
-  //funkcja do generowania losowej liczby z zakresu od min do max
-  function randomNumber(min : number, max : number) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-  //zwykla petla do wypelnienia tablicy losowymi liczbami z powyzszej funkcji
-  for(let i=0; i<100; i++){
-    liczby.push(randomNumber(0, 1000))
-  }
-  //mapowanie tablicy liczb podstawowe
-  var liczbyWysw = liczby.map(liczba => <Text> {liczba} </Text> )
-  //sortowanie
-  const liczbySort = liczby.sort((a,b)=>a-b)
-  var liczbyWyswSort = liczbySort.map(liczba => <Text> {liczba} </Text> )
-
-  const liczbySortDesc = liczby.sort((a,b)=>b-a)
-  var liczbyWyswSortDesc = liczbySortDesc.map(liczba => <Text> {liczba} </Text> )
-
+  const [text, onChangeText] = React.useState("");
+  const [text2, onChangeText2] = React.useState("");
+  const [focus, setFocus] = React.useState(false)
+  const [text3, onChangeText3] = React.useState("");
   return (
     <View style={style.container}>
       <View style={style.galeria}>  
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%', color:'blue' }}>
+        Pole 1 Stany, wyswietlanie
+      </Text>
+      <TextInput
+        placeholder='Wpisz coś'
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%' }}>
+        Wpisany tekst: {text}
+      </Text>
 
-          <Text style={{ fontSize: 20 }}>
-           Wygenerowane liczby:
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWysw}</>}
-          </View>
-
-          <Text style={{ fontSize: 20 }}>
-           Posortowane asc:
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWyswSort}</>}
-          </View>
-
-          <Text style={{ fontSize: 20 }}>
-           Posortowane desc:
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWyswSortDesc}</>}
-          </View>
-           
-      
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%', color: 'red' }}>
+        Pole 2, Klawiatura numeryczna, max znakow - 9, zmiana stylu przy blur i focus
+      </Text>
+      <TextInput
+        placeholder='Wpisz coś'
+        style={focus ? styles.inputFocus : styles.inputBlur}
+        autoCompleteType="email"
+        keyboardType='number-pad'
+        maxLength={9}
+        onFocus={()=> setFocus(true)}
+        onBlur={()=>setFocus(false)}
+        onChangeText={onChangeText2}
+        value={text2}
+      />
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%' }}>
+        Wpisany tekst: {text2}
+      </Text>
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%', color: 'orange' }}>
+        Pole 3, Multiline + 3 linie + zmiany stylow na blur/focus
+      </Text>
+      <TextInput
+        placeholder='Wpisz coś'
+        style={focus ? styles.inputFocus : styles.inputBlur}
+        multiline= {true}
+        numberOfLines={3}
+        onFocus={()=> setFocus(true)}
+        onBlur={()=>setFocus(false)}
+        onChangeText={onChangeText3}
+        value={text3}
+      />
+      <Text style={{ fontSize: 20, flexWrap: 'wrap', maxWidth: '90%' }}>
+        Wpisany tekst: {text3}
+      </Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 120,
+    margin: 0,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'blue',
+    backgroundColor: 'white'
+  },
+  inputFocus: {
+    width: 120,
+    margin: 0,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'red',
+    backgroundColor: 'beige'
+  },
+  inputBlur: {
+    width: 120,
+    margin: 0,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'darkblue',
+    backgroundColor: 'grey'
+  },
   container: {
     flex: 1,
     alignItems: 'center',
--- renedrowanie-listy-danych-progres/screens/TabTwoScreen.tsx
+++ renedrowanie-listy-danych-progres/screens/TabTwoScreen.tsx
@@ -1,62 +1,21 @@
-import { StyleSheet, Image, ImageBackground } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import React from "react";
+import { StyleSheet, Image, ImageBackground} from 'react-native';
+import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components';
 
 export default function TabTwoScreen() {
   const style = require('./styles');
-  const liczby = [];
-  //funkcja do generowania losowej liczby z zakresu od min do max
-  function randomNumber(min : number, max : number) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-  //zwykla petla do wypelnienia tablicy losowymi liczbami z powyzszej funkcji
-  for(let i=0; i<100; i++){
-    liczby.push(randomNumber(0, 1000))
-  }
-  //mapowanie tablicy liczb podstawowe
-  var liczbyWysw = liczby.map(liczba => <Text> {liczba} </Text> )
-  //sortowanie
-  const liczbySort = liczby.filter(checkEven)
-  var liczbyWyswSort = liczbySort.map(liczba => <Text> {liczba} </Text> )
+   const [selectedIndex, setSelectedIndex] = React.useState(new IndexPath(0));
 
-  const liczbySortDesc = liczby.filter(e => !checkEven(e))
-  var liczbyWyswSortDesc = liczbySortDesc.map(liczba => <Text> {liczba} </Text> )
- 
-  function checkEven(even : number) {
-    return (even % 2) == 0;
-  }
   return (
-    <View style={style.container}>
-      <View style={style.galeria}>  
-
-          <Text style={{ fontSize: 20 }}>
-           Wygenerowane liczby:
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWysw}</>}
-          </View>
-
-          <Text style={{ fontSize: 20 }}>
-           Tylko parzyste (filter):
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWyswSort}</>}
-          </View>
-
-          <Text style={{ fontSize: 20 }}>
-           Tylko nieparzyste (filter):
-          </Text>
-
-          <View style={style.liczby}>
-            {<>{liczbyWyswSortDesc}</>}
-          </View>
-           
-      
-      </View>
-    </View>
+    <Layout style={styles.container} level='1'>
+      <Select
+        selectedIndex={selectedIndex}
+        onSelect={index => setSelectedIndex(index)}>
+        <SelectItem title='Option 1'/>
+        <SelectItem title='Option 2'/>
+        <SelectItem title='Option 3'/>
+      </Select>
+    </Layout>
   );
 }
 
@@ -65,6 +24,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+    color: 'red',
+    width: '100%',
+    height: '100%'
   },
   title: {
     fontSize: 20,
